540d7fd18bad4ab3f828f4201023bacfa83c36c7
chaiwichit <chaiwichit.yuwapat@sato-global.com>
2020-08-03 11:57:33 +0700

    Initial solution

---
+++ SampleKeepLog/SampleKeepLog/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SampleKeepLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKeepLog = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKeepLog
+            // 
+            this.btnKeepLog.Location = new System.Drawing.Point(12, 51);
+            this.btnKeepLog.Name = "btnKeepLog";
+            this.btnKeepLog.Size = new System.Drawing.Size(75, 23);
+            this.btnKeepLog.TabIndex = 0;
+            this.btnKeepLog.Text = "Keep Log";
+            this.btnKeepLog.UseVisualStyleBackColor = true;
+            this.btnKeepLog.Click += new System.EventHandler(this.btnKeepLog_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+            this.saveFileDialog1.OverwritePrompt = false;
+            this.saveFileDialog1.Title = "Save File";
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(12, 25);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(208, 20);
+            this.txtData.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "data to keep";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 183);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnKeepLog);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKeepLog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SampleKeepLog/SampleKeepLog/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleKeepLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKeepLog_Click(object sender, EventArgs e)
+        {
+
+            // Check data
+            if (txtData.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Please input data!");
+                return;
+            }
+
+            // choose folder
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            // keep log
+            using (StreamWriter writer = new StreamWriter(saveFileDialog1.FileName,true,Encoding.UTF8))
+            {
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                string dataToKeep = txtData.Text;
+                writer.WriteLine(timestamp + ": "+ dataToKeep);
+            }
+
+            
+            MessageBox.Show("Keep log completed");
+           
+
+        }
+
+    }
+}
